164a90504a662719d4fd1b0503f70f81bd196c7f
Trenton D. Adams <trenton.d.adams@gmail.com>
2021-04-26 18:12:19 -0600

    reformat parse-backup-args.sh

--- components/parse-backup-args.sh
+++ components/parse-backup-args.sh
@@ -1,33 +1,33 @@
 function parseBackupArgs() {
-# Here's some example parameter handling, -d has no args, -p has an argument
-while :; do
-  case $1 in
-    -h|-\?|--help)
+  # Here's some example parameter handling, -d has no args, -p has an argument
+  while :; do
+    case $1 in
+    -h | -\? | --help)
       echo "show_help=true"
       ;;
-    -b|--bucket)
+    -b | --bucket)
       shift
       printf "s3_bucket_name='%s'\n" "$1"
       echo "unset skip_s3"
       ;;
-    -e|--exclude)
+    -e | --exclude)
       shift
       echo "backup_exclude=('--exclude-from')"
       printf "backup_exclude+=('%s')\n" "$1"
       ;;
-    -p|--path)
+    -p | --path)
       shift
       printf "backup_folder='%s'\n" "$1"
       ;;
-    -s|--source)
+    -s | --source)
       shift
       printf "source_folder+=('%s')\n" "$1"
       ;;
-    -n|--name)
+    -n | --name)
       shift
       printf "backup_name='%s'\n" "$1"
       ;;
-    -r|--recipient)
+    -r | --recipient)
       shift
       printf "gpg_recipient='%s'\n" "$1"
       ;;
@@ -35,11 +35,10 @@ while :; do
       printf "args+=('%s')\n" "$1"
       ;;
     *)
-      break;
+      break
       ;;
     esac
 
     shift
-done
+  done
 }
-
